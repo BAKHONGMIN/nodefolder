46cfaa6a664d91bb13c9fef36337ccf3f5cf87ea
Bak <ghdals2655@naver.com>
2023-08-01 22:32:12 +0900

    Revert "135. REACT 수정 페이지 만들기"
    This reverts commit dfba4ef5f719b262abf7abe3bd8b6110eb02cfc3.

--- client/src/components/SoftwareToolsManage/SoftwareView.js
+++ client/src/components/SoftwareToolsManage/SoftwareView.js
@@ -31,7 +31,7 @@ export default function SoftwareView({ props }) {
   const callSwToolInfoApi = async () => {
     axios
       .post("/api/Swtool?type=list", {
-        is_Swtcode: before_swtcode,
+        is_Swtcode: before_swtcode
       })
       .then((response) => {
         try {
@@ -110,18 +110,15 @@ export default function SoftwareView({ props }) {
         const response = await fetch("/api/Swtool?type=" + type, {
           method: "POST",
           headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json"
           },
-          body: Json_form,
+          body: Json_form
         });
         const body = await response.text();
         if (body === "succ") {
           if (type === "save") {
             sweetalertSucc("Software Tools 등록이 완료되었습니다.", false);
-          } else if (type === "modify") {
-            sweetalertSucc("Software Tools 수정이 완료됐습니다.", false);
           }
-
           const timeout = setTimeout(() => {
             history("/SoftwareList");
           }, 1500);
@@ -142,7 +139,7 @@ export default function SoftwareView({ props }) {
       icon: "success",
       title: title,
       showConfirmButton: showConfirmButton,
-      timer: 1000,
+      timer: 1000
     });
   };
 
@@ -210,12 +207,6 @@ export default function SoftwareView({ props }) {
           <form name="frm" id="frm" action="" method="post">
             <input id="is_Swtcode" type="hidden" name="is_Swtcode" />
             <input id="is_Email" type="hidden" name="is_Email" value="guest" />
-            <input
-              id="is_beforeSwtcode"
-              type="hidden"
-              name="is_beforeSwtcode"
-              value={before_swtcode}
-            />
             <article className="res_w">
               <p className="ment" style={{ textAlign: "right" }}>
                 <span className="red">(*)</span>표시는 필수입력사항 입니다.
