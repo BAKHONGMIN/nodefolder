2f3f306bda60765ba2f376bd650aeab07719c754
Bak <ghdals2655@naver.com>
2023-08-01 22:14:33 +0900

    Revert "Revert "129. 파일, 이미지 업로드 api 만들기""
    This reverts commit 1958fdcf99762245571507af4a65ede1c2b01f3a.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 
-const indexRouter = require("../routes/index");
-const usersRouter = require("../routes/users");
-const swtoolRouter = require("../routes/SwtoolRout");
+var indexRouter = require("../routes/index");
+var usersRouter = require("../routes/users");
+var swtoolRouter = require("../routes/SwtoolRout");
 
 const app = express();
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/Swtool", swtoolRouter);
+app.use("/api/upload", fileuploadRouter);
+
+// express.static 함수를 사용하면 정적파일 경로(/uploads)를 설정할수 있다. 정적파일 경로를 사용하면
+// 파일 사용이 필요할 때 지정한 경로에서만 파일을 찾을 수 있다는 보안상의 이점이 있다. 또 지정경로(/uploads)
+// 를 제외한 짧은 url로 파일 경로를 호출할 수있다.
+app.use(express.static("./uploads"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
