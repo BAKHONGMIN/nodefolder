217d8ad2eb81245bf4b486dbb4e44ebe6a4fb080
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-08-07 22:42:40 +0900

    롤백
    롤백

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -11,8 +11,11 @@ const storage = multer.diskStorage({
     }
   },
   filename: function (req, file, cb) {
+    file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );
     cb(null, moment().format("YYYYMMDDHHmmss") + "_" + file.originalname);
-  },
+  }
 });
 
 const upload = multer({ storage: storage }).single("file");
